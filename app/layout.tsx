028c14e2f1817e52c718795892e294e0262a867d
parintnk <parin.tnk@gmail.com>
2026-03-16 11:02:10 +0700

    feat: add Noto Sans Thai font for Thai text support

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="th">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet" />
+      </head>
       <body className={`${montserrat.variable} ${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
