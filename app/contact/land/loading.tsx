e41f984059aefe7d9136b68578b67f7273b1fe85
parintnk <parin.tnk@gmail.com>
2026-04-01 14:03:40 +0700

    fix: remove loading.tsx files that conflicted with TransitionProvider

--- app/contact/land/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingScreen from "../../components/LoadingScreen";
-
-export default function ContactLandLoading() {
-  return <LoadingScreen />;
-}
--- app/projects/[slug]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingScreen from "../../components/LoadingScreen";
-
-export default function ProjectDetailLoading() {
-  return <LoadingScreen />;
-}
