914bc93177c52f0b8b9de88e924f21667e5826d3
parintnk <parin.tnk@gmail.com>
2026-04-03 10:32:25 +0700

    go

--- app/components/ContactMap.tsx
+++ app/components/ContactMap.tsx
@@ -2,7 +2,7 @@ export default function ContactMap() {
   return (
     <section className="w-full">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3776.7!2d98.9853!3d18.7883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30da3a4cde7e74c7%3A0x3e4f13a3afcf9fe7!2s199%2F1-2%20Mahidol%20Rd%2C%20Nong%20Hoi%2C%20Mueang%20Chiang%20Mai%20District%2C%20Chiang%20Mai%2050000!5e0!3m2!1sen!2sth!4v1700000000000!5m2!1sen!2sth"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3777.9998731886226!2d99.10258329999999!3d18.7535427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30da290b338a1d57%3A0xa95a560921c04def!2sThe%20Urbana%2B6!5e0!3m2!1sen!2sth!4v1775187063577!5m2!1sen!2sth"
         width="100%"
         height="400"
         style={{ border: 0 }}
