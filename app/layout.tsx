6f6c6eb791403f6c597e2dba6665f583dc2678d9
parintnk <parin.tnk@gmail.com>
2026-05-18 13:44:16 +0700

    feat: integrate Google Tag Manager into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import PublicStickyContact from "./components/PublicStickyContact";
 import GoogleTranslateProvider from "./components/GoogleTranslateProvider";
 import TransitionProvider from "./components/TransitionProvider";
 import { createClient } from "@/lib/supabase/server";
+
+const GTM_ID = "GTM-NQBJT65K";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -42,8 +45,23 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet" />
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
       </head>
       <body className={`${montserrat.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <GoogleTranslateProvider />
         <TransitionProvider>
           {children}
